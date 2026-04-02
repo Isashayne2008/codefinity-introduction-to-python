77842e0366b6985b086dad3830abde2a1d8b6aa7
Isashayne2008 <>
2026-04-02 17:15:42 +0000

    Boolean Operators

---
+++ conditional_statements/boolean_operators/main.py
@@ -0,0 +1,8 @@
+# 1. Define the grocery bill total
+total_cost = 25.00
+
+# 2. Compare total_cost to the discount threshold to get a boolean
+discountEligible = total_cost >= 20.00
+
+# 3. Print the message with that boolean
+print("Is the purchase eligible for a discount?", discountEligible)
